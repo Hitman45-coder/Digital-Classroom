37bd7f86c3b377fcbd2718caae3a1221dcfb1b73
Bishal Acharya <dreamchasercoder@gmail.com>
2025-06-07 14:44:49 +0545

    Update ui_main_window.py
    Added more UI features

--- Face/ui_main_window.py
+++ Face/ui_main_window.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'main_window.ui'
+## Form generated from reading UI file 'ui_main_window.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.0
 ##
@@ -15,46 +15,36 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QColumnView, QDialog, QGraphicsView,
-    QGridLayout, QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QHeaderView, QPushButton,
+    QSizePolicy, QTableWidget, QTableWidgetItem, QWidget)
 
 class Ui_Start_2(object):
     def setupUi(self, Start_2):
         if not Start_2.objectName():
             Start_2.setObjectName(u"Start_2")
-        Start_2.resize(407, 330)
-        self.gridLayout = QGridLayout(Start_2)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.graphicsView = QGraphicsView(Start_2)
-        self.graphicsView.setObjectName(u"graphicsView")
-
-        self.gridLayout.addWidget(self.graphicsView, 0, 0, 5, 1)
-
-        self.Start = QPushButton(Start_2)
-        self.Start.setObjectName(u"Start")
-
-        self.gridLayout.addWidget(self.Start, 0, 1, 1, 1)
-
-        self.pushButton_2 = QPushButton(Start_2)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-
-        self.gridLayout.addWidget(self.pushButton_2, 1, 1, 1, 1)
-
-        self.pushButton_3 = QPushButton(Start_2)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-
-        self.gridLayout.addWidget(self.pushButton_3, 2, 1, 1, 1)
-
-        self.pushButton = QPushButton(Start_2)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.gridLayout.addWidget(self.pushButton, 3, 1, 1, 1)
-
-        self.columnView = QColumnView(Start_2)
-        self.columnView.setObjectName(u"columnView")
-
-        self.gridLayout.addWidget(self.columnView, 4, 1, 1, 1)
-
+        Start_2.resize(1129, 716)
+        self.Stop_Button = QPushButton(Start_2)
+        self.Stop_Button.setObjectName(u"Stop_Button")
+        self.Stop_Button.setGeometry(QRect(630, 80, 421, 29))
+        self.Add_Button = QPushButton(Start_2)
+        self.Add_Button.setObjectName(u"Add_Button")
+        self.Add_Button.setGeometry(QRect(630, 180, 421, 29))
+        self.Mail_Button = QPushButton(Start_2)
+        self.Mail_Button.setObjectName(u"Mail_Button")
+        self.Mail_Button.setGeometry(QRect(630, 130, 421, 29))
+        self.Remove_Button = QPushButton(Start_2)
+        self.Remove_Button.setObjectName(u"Remove_Button")
+        self.Remove_Button.setGeometry(QRect(630, 230, 421, 29))
+        self.video_container = QWidget(Start_2)
+        self.video_container.setObjectName(u"video_container")
+        self.video_container.setEnabled(True)
+        self.video_container.setGeometry(QRect(15, 20, 601, 629))
+        self.Start_Button = QPushButton(Start_2)
+        self.Start_Button.setObjectName(u"Start_Button")
+        self.Start_Button.setGeometry(QRect(630, 30, 421, 29))
+        self.Attendance = QTableWidget(Start_2)
+        self.Attendance.setObjectName(u"Attendance")
+        self.Attendance.setGeometry(QRect(615, 271, 461, 381))
 
         self.retranslateUi(Start_2)
 
@@ -63,9 +53,10 @@ class Ui_Start_2(object):
 
     def retranslateUi(self, Start_2):
         Start_2.setWindowTitle(QCoreApplication.translate("Start_2", u"Dialog", None))
-        self.Start.setText(QCoreApplication.translate("Start_2", u"Start", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Start_2", u"Stop", None))
-        self.pushButton_3.setText(QCoreApplication.translate("Start_2", u"Attendance", None))
-        self.pushButton.setText(QCoreApplication.translate("Start_2", u"Mail", None))
+        self.Stop_Button.setText(QCoreApplication.translate("Start_2", u"Stop", None))
+        self.Add_Button.setText(QCoreApplication.translate("Start_2", u"Add Face", None))
+        self.Mail_Button.setText(QCoreApplication.translate("Start_2", u"Mail", None))
+        self.Remove_Button.setText(QCoreApplication.translate("Start_2", u"Remove Face", None))
+        self.Start_Button.setText(QCoreApplication.translate("Start_2", u"Start", None))
     # retranslateUi
 
